cb8f96b1c26c671d1067249b725bab7d750ff91e
Dafydd Giddins <dafydd.giddins@live.co.uk>
2015-03-31 09:22:14 +0100

    First sandbox project - ntextcat

---
+++ src/Sandbox/NTextCat-Sandbox/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IvanAkcheurov.NTextCat.Lib;
+using NUnit.Framework;
+
+namespace NTextCat_Sandbox
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TestNTextCat()
+        {
+            var factory = new RankedLanguageIdentifierFactory();
+            var identifier = factory.Load("Core14.profile.xml");
+            IEnumerable<Tuple<LanguageInfo, double>> res = identifier.Identify("your text to get its language identified");
+
+            Assert.That(res.Count(), Is.GreaterThan(0));
+        }
+    }
+}
